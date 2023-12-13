f7a7908a1f3f371d2f1291d9ca4fe448ea08d920
openmymai <maicmi@me.com>
2023-12-13 13:08:44 +0700

    eCommerce Fullstack

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+// import HomeScreen from './screens/HomeScreen';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
