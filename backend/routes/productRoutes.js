fbb34c206dfe6f2ee4fef7c89a48a0d6b5a27fb1
openmymai <maicmi@me.com>
2023-12-17 13:19:34 +0700

    Fix productRoutes.js - router.get to router.route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js';
 import {
   getProducts,
   getProductById,
 } from '../controller/productController.js';
 
-router.get('/').get(getProducts);
-router.get('/:id').get(getProductById);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
